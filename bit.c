2a8c6885069162d1a8e9c6a069dfca1cf532c155
Ross Light <ross@zombiezen.com>
2010-10-01 08:17:21 -0700

    Change bit.c includes (vxWorks doesn't have stdint.h)

--- bit.c
+++ bit.c
@@ -38,7 +38,7 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int64 uint64_t;
 #else
-#include <stdint.h>
+#include <sys/types.h>
 #endif
 
 typedef int32_t SBits;
